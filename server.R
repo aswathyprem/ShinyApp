d51b775bbcc557eb4a6c4e579e00d3d5b5bb95f0
aswathyprem <aswathypremblogs@gmail.com>
2016-06-30 12:49:14 +0200

    Updated comments

--- server.R
+++ server.R
@@ -1,53 +1,40 @@
 library(shiny)
-library(datasets)
 
 # Define server logic required to summarize and view the selected
-# dataset
 shinyServer(function(input, output) {
   
   v <- reactiveValues(data = NULL)
   
+  # List the countries from rpanama dataset
   observeEvent(input$countries,{
     v$data <- Entities[17]
   })
   
+  # List the companies from rpanama dataset
   observeEvent(input$companies,{
     
     v$data <- Entities[1]
     
   })
   
+  # List the involved people from rpanam dataset
   observeEvent(input$people,{
     
     v$data <- Officers[1]
     
   })
   
-  # The output$caption is computed based on a reactive expression
-  # that returns input$caption. When the user changes the
-  # "caption" field:
-  #
-  #  1) This function is automatically called to recompute the 
-  #     output 
-  #  2) The new caption is pushed back to the browser for 
-  #     re-display
-  # 
-  # Note that because the data-oriented reactive expressions
-  # below don't depend on input$keyword, those expressions are
-  # NOT called when input$caption changes.
+ #output$keyword takes the input$keyword and displays the serach results
   output$keyword <- renderText({
     input$keyword
   })
   
-  # The output$summary depends on the serch keyword in reactive
-  # expression, so will be re-executed whenever keyword is
-  # invalidated
-  # (i.e. whenever the input$keyword changes)
+ #output$summary takes the input$keyword and displays the serach results
   output$summary <- renderPrint({
     Entities[grep(input$keyword,rownames(Entities)),]
   })
   
-  # The output$view depends on both the databaseInput reactive
+  # The output$view depends on both the actionLink clicked in reactive
   # expression, number of items set to 100
   
   output$view <- renderTable({
