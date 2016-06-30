61c55bd688492acb2e3f7cbac8e3fff8f8c6f75f
aswathyprem <aswathypremblogs@gmail.com>
2016-06-30 18:48:27 +0200

    incorporated tabs

--- ui.R
+++ ui.R
@@ -10,7 +10,8 @@ shinyUI(fluidPage(
   
   sidebarLayout(
     sidebarPanel(
-      textInput("keyword", "Search for stories", "Data Summary"),
+      textInput("keyword", "Search for stories", NULL),
+      
       
       actionLink("countries", "Show invloved countries"),
       br(),
@@ -23,16 +24,25 @@ shinyUI(fluidPage(
     ),
     
     
-  
+    # Show the caption, a summary of the dataset and an HTML 
+    # table with the requested number of observations
     mainPanel(
       #displays the keyword searched
+      
       h3(textOutput("keyword", container = span)),
       
-      #Displays data for the keyword
-      verbatimTextOutput("summary"), 
+      #Display the tabular output
+      tabsetPanel(type = "tabs", 
+                  
+                  tabPanel("Summary", verbatimTextOutput("summary")), 
+                  tabPanel("Table", tableOutput("view")),
+                  tabPanel("Plot", plotOutput("plot")) 
+      )
       
-      #Displays data based on the links clicked
-      tableOutput("view")
+      
+      #verbatimTextOutput("summary"), 
+      
+      #tableOutput("view")
       
     )
     
