c40d0b936547391da43b286331b8aa0553741905
aswathyprem <aswathypremblogs@gmail.com>
2016-06-30 12:51:49 +0200

    Updated comments

--- ui.R
+++ ui.R
@@ -6,18 +6,11 @@ shinyUI(fluidPage(
   # Application title
   titlePanel("Panama Search"),
   
-  # Sidebar with controls to provide a caption, select a dataset,
-  # and specify the number of observations to view. Note that
-  # changes made to the caption in the textInput control are
-  # updated in the output area immediately as you type
+  # Sidebar with controls to provide a search box, and links to display data from rpanama,
+  
   sidebarLayout(
     sidebarPanel(
       textInput("keyword", "Search for stories", "Data Summary"),
-      
-      #selectInput("dataset", "Choose a dataset:", 
-      #choices = c("Companies", "Countries", "People")),
-      
-      #numericInput("obs", "Number of observations to view:", 10),
       
       actionLink("countries", "Show invloved countries"),
       br(),
@@ -30,13 +23,15 @@ shinyUI(fluidPage(
     ),
     
     
-    # Show the caption, a summary of the dataset and an HTML 
-    # table with the requested number of observations
+  
     mainPanel(
+      #displays the keyword searched
       h3(textOutput("keyword", container = span)),
       
+      #Displays data for the keyword
       verbatimTextOutput("summary"), 
       
+      #Displays data based on the links clicked
       tableOutput("view")
       
     )
